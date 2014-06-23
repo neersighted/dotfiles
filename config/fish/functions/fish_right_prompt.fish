3a6f913ba011ce19e9c85d8bb226246a70699078
neersighted <n@neersighted.com>
2014-06-22 20:16:45 -0700

    [fish] awesome new, minimal prompt (even more minimal if you set minimal 1)

---
+++ config/fish/functions/fish_right_prompt.fish
@@ -0,0 +1,41 @@
+set -g __fish_git_prompt_show_informative_status 1
+set -g __fish_git_prompt_hide_untrackedfiles 1
+
+set -g __fish_git_prompt_color_branch magenta bold
+set -g __fish_git_prompt_showupstream "informative"
+set -g __fish_git_prompt_char_upstream_ahead "↑"
+set -g __fish_git_prompt_char_upstream_behind "↓"
+set -g __fish_git_prompt_char_upstream_prefix ""
+
+set -g __fish_git_prompt_char_conflictedstate "x"
+set -g __fish_git_prompt_char_dirtystate "~"
+set -g __fish_git_prompt_char_untrackedfiles "?"
+set -g __fish_git_prompt_char_stagedstate "+"
+set -g __fish_git_prompt_char_cleanstate "o"
+
+set -g __fish_git_prompt_color_dirtystate blue
+set -g __fish_git_prompt_color_stagedstate yellow
+set -g __fish_git_prompt_color_invalidstate red
+set -g __fish_git_prompt_color_untrackedfiles normal
+set -g __fish_git_prompt_color_cleanstate green bold
+
+function fish_right_prompt --description 'simple right prompt'
+  set -l last_status $status
+
+  if set -q minimal; return; end
+
+  set_color normal
+
+  set_color $fish_color_cwd
+  echo -n " "(prompt_pwd)
+
+  set_color normal
+  echo -n (__fish_git_prompt)
+
+  if not test $last_status -eq 0
+    set_color $fish_color_error
+    echo -n " [$last_status]"
+  end
+
+  set_color normal
+end
