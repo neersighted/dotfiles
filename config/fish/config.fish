2329b0d0a2eab3528eaad6b51ea6c9aa387ce02b
Bjorn Neergaard <bjorn@neersighted.com>
2015-03-05 21:49:15 -0800

    [dircolors] add dircolors

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,9 +1,21 @@
 #
-# TTY
+# Terminal
 #
 
 # Fix the keypad.
 tput smkx
+
+# Enable colors in ls.
+source (dircolors -c ~/.dircolorsrc|psub)
+
+# Enable color in less.
+set -xU LESS_TERMCAP_mb (printf "\e[01;31m")      # begin blinking
+set -xU LESS_TERMCAP_md (printf "\e[01;31m")      # begin bold
+set -xU LESS_TERMCAP_me (printf "\e[0m")          # end mode
+set -xU LESS_TERMCAP_se (printf "\e[0m")          # end standout-mode
+set -xU LESS_TERMCAP_so (printf "\e[01;44;33m")   # begin standout-mode - info box
+set -xU LESS_TERMCAP_ue (printf "\e[0m")          # end underline
+set -xU LESS_TERMCAP_us (printf "\e[01;32m")      # begin underline
 
 #
 # Paths
