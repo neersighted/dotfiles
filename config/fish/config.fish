7273ca63ed727309cdb24d944f4ddb102dc3b530
Bjorn Neergaard <bjorn@neersighted.com>
2015-05-15 17:14:40 -0700

    [...a lot] changes I should have committed

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -10,13 +10,16 @@ source (dircolors --c-shell ~/.dircolorsrc|psub)
 
 # Enable color in less.
 set --global --export LESS -R
-set -xU LESS_TERMCAP_mb (printf "\e[01;31m")      # begin blinking
-set -xU LESS_TERMCAP_md (printf "\e[01;31m")      # begin bold
-set -xU LESS_TERMCAP_me (printf "\e[0m")          # end mode
-set -xU LESS_TERMCAP_se (printf "\e[0m")          # end standout-mode
-set -xU LESS_TERMCAP_so (printf "\e[01;44;33m")   # begin standout-mode - info box
-set -xU LESS_TERMCAP_ue (printf "\e[0m")          # end underline
-set -xU LESS_TERMCAP_us (printf "\e[01;32m")      # begin underline
+set --global --export LESS_TERMCAP_mb (printf "\e[01;31m")      # begin blinking
+set --global --export LESS_TERMCAP_md (printf "\e[01;31m")      # begin bold
+set --global --export LESS_TERMCAP_me (printf "\e[0m")          # end mode
+set --global --export LESS_TERMCAP_se (printf "\e[0m")          # end standout-mode
+set --global --export LESS_TERMCAP_so (printf "\e[01;44;33m")   # begin standout-mode - info box
+set --global --export LESS_TERMCAP_ue (printf "\e[0m")          # end underline
+set --global --export LESS_TERMCAP_us (printf "\e[01;32m")      # begin underline
+
+# Fix Java windows in DWM.
+set --global --export _JAVA_AWT_WM_NONREPARENTING 1
 
 #
 # Environment
