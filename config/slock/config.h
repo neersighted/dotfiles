93c021b1e1f8ab115b75d3485e03e661e5ed0197
Bjorn Neergaard <bjorn@neersighted.com>
2015-08-05 09:42:15 -0700

    Fixes all around

---
+++ config/slock/config.h
@@ -0,0 +1,6 @@
+static const char *colorname[NUMCOLS] = {
+	"black",   /* after initialization */
+	"#268bd2", /* during input */
+	"#dc322f", /* failed/cleared the input */
+};
+static const Bool failonclear = True;
