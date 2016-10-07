141e8f9fbac6f2ac1b1531ef4e2f60b63d873fdf
Bjorn Neergaard <bjorn@neersighted.com>
2016-10-07 00:10:31 -0500

    Remove old gpg-agent code

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,11 +1,9 @@
-
 function fish_prompt --description 'left prompt'
-
   if test -n "$SSH_CLIENT"; or test -n "$SSH_TTY"
     set_color $fish_color_hostname
     printf '%s ' (hostname -s)
     set_color normal
   end
 
-  printf "λ "
+  printf 'λ '
 end
