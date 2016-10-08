1f444f30c0166596bd2345d131ac1f5d7ff9946b
Bjorn Neergaard <bjorn@neersighted.com>
2016-10-08 18:23:45 -0500

    Remove unused code

--- config/fish/colors.fish
+++ config/fish/colors.fish
@@ -1,20 +1,3 @@
-set -l base03 002b36
-set -l base02 073642
-set -l base01 586e75
-set -l base00 657b83
-set -l base0 839496
-set -l base1 93a1a1
-set -l base2 eee8d5
-set -l base3 fdf6e3
-set -l yellow b58900
-set -l orange cb4b16
-set -l red dc322f
-set -l magenta d33682
-set -l violet 6c71c4
-set -l blue 268bd2
-set -l cyan 2aa198
-set -l green 859900
-
 set -l base03 "--bold black"
 set -l base02 "black"
 set -l base01 "--bold green"
