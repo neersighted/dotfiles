c7ac4c62245144ce6b20ae4e7355bc35eb587182
Bjorn Neergaard <bjorn@neersighted.com>
2016-10-09 13:29:58 -0500

    neovim by default, custom neovim arch package

--- config/fish/aliases.fish
+++ config/fish/aliases.fish
@@ -5,7 +5,10 @@ else
   alias ls 'ls -Alh -G'
 end
 
-# Open in existing Gvim.
-function gvim
+if which nvim >/dev/null 2>&1 # Neovim if we got it.
+  alias vim nvim
+end
+
+function gvim # Open in existing Gvim.
   test -n "$argv"; and command gvim --remote-silent $argv; or command gvim
 end
