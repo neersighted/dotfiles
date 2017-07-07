4b70ba504b6074e616d33e2227a80ca4613ef90c
Bjorn Neergaard <bjorn@neersighted.com>
2017-07-06 19:49:05 -0500

    Clean out now-unused cruft; GTK theme tryouts; quickterm script; etc

--- config/slock/config.h
+++
@@ -1,9 +0,0 @@
-static const char *user  = "nobody";
-static const char *group = "nobody";
-
-static const char *colorname[NUMCOLS] = {
-	"black",   /* after initialization */
-	"#002b36", /* during input */
-	"#dc322f", /* failed/cleared the input */
-};
-static const Bool failonclear = True;
