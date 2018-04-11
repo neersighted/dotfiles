17a6f77f9b5d8277b5850fad8f7f1cdd651fb159
Bjorn Neergaard <bjorn@neersighted.com>
2018-04-11 07:41:09 -0600

    Oops, messed it up the other way

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -51,8 +51,8 @@ if status --is-interactive
     and not set -q SSH_CLIENT; and not set -q MOSH; and not set -q TMUX
 
     # exec X (on tty1 only)
-    test -z "$DISPLAY" -a "$XDG_VTNR" = 1
-      or exec startx
+    not set -q "$DISPLAY"; and test "$XDG_VTNR" -eq 1
+      and exec startx
 
     # start tmux (or attach if already running)
     if command -s tmux >/dev/null ^&1
