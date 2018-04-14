0d5cc3fedf54bddc180d2ed42c2be2448313e770
Bjorn Neergaard <bjorn@neersighted.com>
2018-04-14 15:26:46 -0600

    provide ls with exa

---
+++ config/fish/functions/ls.fish
@@ -0,0 +1,11 @@
+function ls
+  if type -q exa >/dev/null 2>&1
+    command exa $param $argv
+  else
+    set -l param --color=auto
+    if isatty 1
+      set param $param --indicator-style=classify
+    end
+    command ls $param $argv
+  end
+end
