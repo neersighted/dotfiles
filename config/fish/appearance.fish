1ead2b40435b617308d7e2ec1f9fcb7f7d278371
Bjorn Neergaard <bjorn@neersighted.com>
2018-04-14 20:12:20 -0600

    simplify color config

--- config/fish/appearance.fish
+++ config/fish/appearance.fish
@@ -37,7 +37,7 @@ set -g __fish_git_prompt_color_untrackedfiles $color_magenta
 
 # fzf
 if set -q fish_term24bit
-  set -x FZF_DEFAULT_OPTS "--color=fg:$color_base0,bg:$color_base03,hl:$color_base2,fg+:$color_base0,bg+:$color_base02,hl+:$color_magenta --color=info:$color_cyan,prompt:$color_base0,pointer:$color_base3,marker:$color_base3,spinner:$color_cyan"
+  set -x FZF_DEFAULT_OPTS "--color=fg:#839496,bg:#002b36,hl:#eee8d5,fg+:#839496,bg+:#073642,hl+:#d33682 --color=info:#2aa198,prompt:#839496,pointer:#fdf6e3,marker:#fdf6e3,spinner:#2aa198"
 else
   set -x FZF_DEFAULT_OPTS "--color=fg:12,bg:8,hl:7,fg+:12,bg+:0,hl+:5 --color=info:6,prompt:12,pointer:15,marker:15,spinner:6"
 end
@@ -68,3 +68,5 @@ set -g fish_color_cancel $color_base01
 set -g fish_color_search_match --background=$color_base02
 set -g fish_color_selection $color_base3 --background=$color_cyan
 set -g fish_color_valid_path --underline
+
+# vi:ft=fish:
--- config/fish/colors.fish
+++ config/fish/colors.fish
@@ -1,39 +1,21 @@
 # enable 24bit color
 set -g fish_term24bit 1
 
-if set -q fish_term24bit
-  set -g color_base03  '#002b36'
-  set -g color_base02  '#073642'
-  set -g color_base01  '#586e75'
-  set -g color_base00  '#657b83'
-  set -g color_base0   '#839496'
-  set -g color_base1   '#93a1a1'
-  set -g color_base2   '#eee8d5'
-  set -g color_base3   '#fdf6e3'
-  set -g color_yellow  '#b58900'
-  set -g color_orange  '#cb4b16'
-  set -g color_red     '#dc322f'
-  set -g color_magenta '#d33682'
-  set -g color_violet  '#6c71c4'
-  set -g color_blue    '#268bd2'
-  set -g color_cyan    '#2aa198'
-  set -g color_green   '#859900'
-else
-  # currently unused, but here for backwards compat
-  set -g color_base03  brblack
-  set -g color_base02  black
-  set -g color_base01  brgreen
-  set -g color_base00  bryellow
-  set -g color_base0   brblue
-  set -g color_base1   brcyan
-  set -g color_base2   white
-  set -g color_base3   brwhite
-  set -g color_yellow  yellow
-  set -g color_orange  brred
-  set -g color_red     red
-  set -g color_magenta magenta
-  set -g color_violet  brmagenta
-  set -g color_blue    blue
-  set -g color_cyan    cyan
-  set -g color_green   green
-end
+set -g color_base03  \#002b36 brblack
+set -g color_base02  \#073642 black
+set -g color_base01  \#586e75 brgreen
+set -g color_base00  \#657b83 bryellow
+set -g color_base0   \#839496 brblue
+set -g color_base1   \#93a1a1 brcyan
+set -g color_base2   \#eee8d5 white
+set -g color_base3   \#fdf6e3 brwhite
+set -g color_yellow  \#b58900 yellow
+set -g color_orange  \#cb4b16 brred
+set -g color_red     \#dc322f red
+set -g color_magenta \#d33682 magenta
+set -g color_violet  \#6c71c4 brmagenta
+set -g color_blue    \#268bd2 blue
+set -g color_cyan    \#2aa198 cyan
+set -g color_green   \#859900 green
+
+# vi:ft=fish:
