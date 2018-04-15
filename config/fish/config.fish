68f6824d6737752f22a0fdf9f001613eaffc9164
Bjorn Neergaard <bjorn@neersighted.com>
2018-04-15 05:21:39 -0600

    attempt to speed up fish launch more; yankee fixes

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,18 +1,26 @@
 # interactive shells (attached to a keyboard)
 if status --is-interactive
-  # programs
-  set -x VISUAL nvim
-  set -x EDITOR nvim
-  set -x TERMINAL alacritty
-  set -x LESS '-R'
-  test (uname) = "Darwin"
-    and set -x BROWSER open
-    or set -x BROWSER firefox-nightly
-  abbr vim nvim # muscle memory
+  # exports are inherited so there's no need to re-export them
+  if status --is-login
+    set -x VISUAL nvim
+    set -x EDITOR nvim
+    set -x TERMINAL alacritty
+    set -x LESS '-R'
+    test (uname) = "Darwin"
+      and set -x BROWSER open
+      or set -x BROWSER firefox-nightly
 
-  # fzf (global)
-  set -x FZF_DEFAULT_COMMAND 'fd --type file'
-  # set -x FZF_DEFAULT_OPTS ''
+    # coreutils
+    type -q dircolors
+      and source (dircolors -c ~/.dircolors|psub)
+
+    # libvirt
+    set -x LIBVIRT_DEFAULT_URI qemu:///system
+
+    # fzf (global)
+    set -x FZF_DEFAULT_COMMAND 'fd --type file'
+    set -x FZF_DEFAULT_OPTS "$FZF_DEFAULT_OPTS --no-bold"
+  end
 
   # fzf (plugin)
   set -g FZF_TMUX 1
@@ -22,8 +30,11 @@ if status --is-interactive
   set -g FZF_CD_COMMAND 'fd --type directory --follow'
   set -g FZF_CD_WITH_HIDDEN_COMMAND 'fd --type directory --follow --hidden --exclude .git'
 
-  # libvirt
-  set -x LIBVIRT_DEFAULT_URI qemu:///system
+  # abbreviations (versioned here so they carry over automatically)
+  if not set -q fish_abbr_version; or test $fish_abbr_version -lt 1
+    abbr vim nvim # muscle memory
+    set -U fish_abbr_version 1
+  end
 
   if uname -r | grep -Fq "Microsoft"
     # connect ssh to the windows ssh-agent
@@ -37,11 +48,12 @@ if status --is-interactive
       and set -x GPG_TTY (tty)
 
     # connect ssh to gpg-agent and inform gpg-agent of our TTY
-    not set -q SSH_CLIENT; and not set -q MOSH
-      and type -q gpgconf >/dev/null 2>&1
-      and set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
-      and type -q gpg-connect-agent >/dev/null 2>&1
-      and gpg-connect-agent updatestartuptty /bye >/dev/null 2>&1
+    if not set -q SSH_CLIENT; and not set -q MOSH
+      type -q gpgconf
+        and set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
+      type -q gpg-connect-agent
+        and gpg-connect-agent updatestartuptty /bye >/dev/null 2>&1
+    end
   end
 
   # autostart actions (login, local, root shells only)
@@ -54,7 +66,7 @@ if status --is-interactive
       and exec startx
 
     # start tmux (or attach if already running)
-    if type -q tmux >/dev/null 2>&1
+    if type -q tmux
       set -l session (hostname)
       tmux has-session -t $session
         and tmux new-session -t $session \; set-option destroy-unattached
