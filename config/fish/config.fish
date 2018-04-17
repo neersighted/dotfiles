60f4ac0df64a566ad732adfbf5a5866713ceda0c
Bjorn Neergaard <bjorn@neersighted.com>
2018-04-16 18:36:56 -0600

    fix fish supression of tmux messages

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -77,7 +77,7 @@ if status --is-interactive
     # start our main tmux session (on a pts)
     if type -q tmux; and not set -q TMUX
       set -l session (prompt_hostname)
-      if tmux has-session -t $session >/dev/null
+      if tmux has-session -t $session 2>/dev/null
         tmux new-session -t $session\; set-option destroy-unattached
       else
         tmux new-session -s $session
