bd053e48c58f27c4485788d9c7ee289a56036ef6
Bjorn Neergaard <bjorn@neersighted.com>
2018-06-02 16:19:49 -0600

    Rework docker relay

--- config/fish/functions/docker.fish
+++ config/fish/functions/docker.fish
@@ -1,7 +1,6 @@
 function docker -d "docker client"
   if set -q WSL; and type -q npiperelay.exe; and not test -e /var/run/docker.sock
-    sudo socat UNIX-LISTEN:/var/run/docker.sock,fork,group=docker,umask=007 EXEC:(which npiperelay.exe)" -ep -s //./pipe/docker_engine",nofork &
-    disown %last
+    sudo sh -c "socat UNIX-LISTEN:/var/run/docker.sock,fork,group=docker,umask=007 EXEC:\""(which npiperelay.exe)" -ep -s //./pipe/docker_engine\",nofork &"
   end
   command docker $argv
 end
