db9c4d05885764a08da1ae8e27368cecc0dd5965
Bjorn Neergaard <bjorn@neersighted.com>
2018-11-21 04:10:31 -0700

    Rework docker-relay strategy

--- config/fish/functions/docker.fish
+++ config/fish/functions/docker.fish
@@ -3,7 +3,7 @@ function docker -d 'docker client'
     and command -sq docker.exe
     and command -sq npiperelay.exe
 
-    sudo sh -c 'socat UNIX-LISTEN:/var/run/docker.sock,fork,group=docker,umask=007 EXEC:"'(command -s npiperelay.exe)' -ep -s //./pipe/docker_engine",nofork &'
+    sudo (command -s docker-relay) (command -s npiperelay.exe)
   end
   command docker $argv
 end
