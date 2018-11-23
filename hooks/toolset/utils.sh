8b2dbe6b863a43ecbe51d6e626c94013f42fd951
Bjorn Neergaard <bjorn@neersighted.com>
2018-11-23 16:38:02 -0700

    Add httpie

--- hooks/toolset/utils.sh
+++ hooks/toolset/utils.sh
@@ -4,18 +4,23 @@ toolset_subsection "Utils"
 cargo_install "bat"
 cargo_install "exa"
 cargo_install "fd-find"
-cargo_install "hyperfine"
 cargo_install "ripgrep"
+
+# Data Processing
+go_get "github.com/simeji/jid/cmd/jid"
+go_get "mvdan.cc/xurls/cmd/xurls"
+
+# Network Tools
+pipx_install "httpie"
+
+# Command Runners
+cargo_install "hyperfine"
 cargo_install "watchexec"
 
 # Code Counters
 go_get "github.com/boyter/scc"
 cargo_install "tokei"
 
-# Data Processing
-go_get "github.com/simeji/jid/cmd/jid"
-go_get "mvdan.cc/xurls/cmd/xurls"
-
-# Shell Tools
+# Misc Tools
 pipx_install "asciinema"
 pipx_install "shell-functools"
