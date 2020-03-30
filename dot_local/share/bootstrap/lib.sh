897998643568d583cd25c61f9f00c64265e69131
Bjorn Neergaard <bjorn@neersighted.com>
2020-03-30 01:03:25 -0600

    shfmt

--- dot_local/share/bootstrap/lib.sh
+++ dot_local/share/bootstrap/lib.sh
@@ -75,19 +75,19 @@ export PATH="$CARGO_HOME/bin:$PATH"
 ###
 
 section() {
-  printf "\\033[0;33m%s\\033[0m\\n" "$@"
+  printf '\033[0;33m%s\033[0m\n' "$@"
 }
 
 info() {
-  printf "\\033[0;32m%s\\033[0m\\n" "$@"
+  printf '\033[0;32m%s\033[0m\n' "$@"
 }
 
 important() {
-  printf "\\033[0;35m%s\\033[0m\\n" "$@"
+  printf '\033[0;35m%s\033[0m\n' "$@"
 }
 
 error() {
-  printf "\\033[0;31m%s\\033[0m\\n" "$@"
+  printf '\033[0;31m%s\033[0m\n' "$@"
   exit 1
 }
 
