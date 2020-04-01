63ee0b5852d6a8a8ddcd2f63fba0a4805ee58e73
Bjorn Neergaard <bjorn@neersighted.com>
2020-04-01 08:35:33 -0600

    pkg-provies and remove pkgfile

--- dot_config/fish/conf.d/command_not_found.fish
+++ dot_config/fish/conf.d/command_not_found.fish
@@ -1,23 +1,22 @@
 status is-interactive; or exit
 
-if command -q pkgfile # Arch
+if command -q pacman # Arch
   function __fish_command_not_found_handler -a cmd -e fish_command_not_found
-    if set pkgs (pkgfile -bv -- $cmd)
+    __fish_default_command_not_found_handler $cmd
+
+    if set pkgs (pacman -Fq /usr/bin/$cmd)
       printf '%s may be found in the following packages:\n' $cmd
       for pkg in $pkgs
         printf '  %s\n' $pkg
       end
-    else
-      __fish_default_command_not_found_handler $cmd
     end
   end
 else if command -q brew # macOS
   function __fish_command_not_found_handler -a cmd -e fish_command_not_found
+    __fish_default_command_not_found_handler $cmd
 
     if brew which-formula --explain $cmd | read -z text
         printf '%s' $text
-    else
-      __fish_default_command_not_found_handler $cmd
     end
   end
 end
