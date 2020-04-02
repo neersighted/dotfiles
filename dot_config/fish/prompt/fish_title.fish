dfe2163a3c6d3a1763fe85766988bcddc476d0f3
Bjorn Neergaard <bjorn@neersighted.com>
2020-04-02 11:43:55 -0600

    fix up packages, ssh title

--- dot_config/fish/prompt/fish_title.fish
+++ dot_config/fish/prompt/fish_title.fish
@@ -2,6 +2,10 @@ function fish_title -a cmd
   set basename (string replace -r '^.*/' '' $PWD)
   set cwd (short_home $PWD)
 
+  if is_ssh
+    printf '(%s) ' (prompt_hostname)
+  end
+
   if test -n "$cmd"
     printf '%s: %s' $basename $cmd
   else
