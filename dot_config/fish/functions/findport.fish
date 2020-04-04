a9c948052d30a6b9cb77cf0be7172cf56e511e75
Bjorn Neergaard <bjorn@neersighted.com>
2020-04-04 11:57:35 -0600

    findport command

---
+++ dot_config/fish/functions/findport.fish
@@ -0,0 +1,9 @@
+if is_freebsd
+  function findport -a q
+    test -n "$q"; or set q .
+    fd --fixed-strings --full-path --type directory --max-depth 2 $q /usr/ports \
+      | sd '/usr/ports/' '' \
+      | grep $q
+  end
+end
+
