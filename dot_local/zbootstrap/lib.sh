ee64fcc3f6a53abcce508ed32124ee75cb90f22a
Bjorn Neergaard <bjorn@neersighted.com>
2020-04-04 01:13:15 -0600

    messaging update

--- dot_local/zbootstrap/lib.sh
+++ dot_local/zbootstrap/lib.sh
@@ -92,15 +92,15 @@ export PATH="$CARGO_HOME/bin:$PATH"
 ###
 
 section() {
-  printf '\033[0;33m%s\033[0m\n' "$@"
-}
-
-info() {
-  printf '\033[0;32m%s\033[0m\n' "$@"
+  printf '\033[0;35m%s\033[0m\n' "$@"
 }
 
 important() {
-  printf '\033[0;35m%s\033[0m\n' "$@"
+  printf '\033[0;32m%s\033[0m\n' "$@"
+}
+
+info() {
+  printf '\033[0;33m%s\033[0m\n' "$@"
 }
 
 error() {
