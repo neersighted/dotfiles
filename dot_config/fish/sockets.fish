ce7afa3fd1c36bf513c81cba8dd064702c3d26c8
Bjorn Neergaard <bjorn@neersighted.com>
2020-05-05 10:28:47 -0600

    wsl: greatly simplify integration using wsl2-ssh-pageant

--- dot_config/fish/sockets.fish
+++ dot_config/fish/sockets.fish
@@ -3,12 +3,19 @@ is_ssh; and exit
 set -x GPG_TTY $tty
 
 if is_wsl
-  # make sure gpg-agent is up
-  gpg-connect-agent.exe /bye &>/dev/null
-  # connect ssh to windows gpg-agent via weasel-pageant
-  set -q SSH_AUTH_SOCK; or weasel-pageant -qrb -a $HOME/.ssh/weasel-pageant.sock -c | source
-  # connect gpg to windows gpg-agent via socat/npiperelay
-  test -S $GNUPGHOME/S.gpg-agent; or wsl-gpg-relay
+  set -x SSH_AUTH_SOCK $HOME/.ssh/ssh-agent.sock
+
+  # connect ssh to windows gpg-agent via wsl2-ssh-pageant
+  if not ss -a | string match -rq $SSH_AUTH_SOCK
+    rm -f $SSH_AUTH_SOCK
+    setsid socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:'wsl2-ssh-pageant.exe' &>/dev/null &; disown
+  end
+
+  # connect gpg to windows gpg-agent via wsl2-ssh-pageant
+  if not ss -a | string match -rq $GNUPGHOME/S.gpg-agent
+    rm -f $GNUPGHOME/S.gpg-agent
+    setsid socat UNIX-LISTEN:$GNUPGHOME/S.gpg-agent,fork EXEC:'wsl2-ssh-pageant.exe --gpg S.gpg-agent' &>/dev/null &; disown
+  end
 else if not string match -rq 'S.gpg-agent.ssh$' $SSH_AUTH_SOCK
   # plumb ssh to gpg-agent
   set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
