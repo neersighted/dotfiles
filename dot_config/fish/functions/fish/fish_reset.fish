6a2249741ad85a1d2237acf4a32a5f1d9483dd28
Bjorn Neergaard <bjorn@neersighted.com>
2020-05-05 10:27:26 -0600

    fish: misc function cleanup

--- dot_config/fish/functions/fish/fish_reset.fish
+++ dot_config/fish/functions/fish/fish_reset.fish
@@ -2,7 +2,7 @@ function fish_reset --description 'reset local fish configuration'
   # erase all universal variables
   for setting in $fish_settings
     set variable (string split ' ' $setting)[1]
-    echo "reset $variable"
+    printf 'reset %s\n' $variable
     set -eU $variable
   end
 
--- dot_config/fish/functions/upcate.fish
+++ dot_config/fish/functions/upcate.fish
@@ -5,7 +5,7 @@ function upcate -a target path -d 'locate a target upwards in the directory tree
 
   while test "$path" != /
     if test -e $path/$target
-      echo $path/$target
+      printf '%s/%s' $path $target
       return
     else
       set path (builtin realpath $path/..)
