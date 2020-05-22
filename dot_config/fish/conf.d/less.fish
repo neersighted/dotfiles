50ccdcc3f0bf79198c7b39aae8617bb078fa5421
Bjorn Neergaard <bjorn@neersighted.com>
2020-05-22 02:40:06 -0600

    replicator/fish: remove lesspipe

--- dot_config/fish/conf.d/less.fish
+++
@@ -1,9 +0,0 @@
-if not set -q LESSOPEN
-  if set -l lesspipe (command -s lesspipe)
-    set -x LESSOPEN "$lesspipe %s"
-    set -x LESSCLOSE "$lesspipe %s %s"
-  else if set -l lesspipe (command -s lesspipe.sh)
-    set -x LESSOPEN "|$lesspipe %s"
-    set -x LESS_ADVANCED_PREPROCESSOR 1
-  end
-end
