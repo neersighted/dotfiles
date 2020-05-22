d5e1bee1fa200bc00a6ecb7047eede062172a3bb
Bjorn Neergaard <bjorn@neersighted.com>
2020-05-21 22:55:17 -0600

    direnv: try a simpler hook

--- dot_config/fish/conf.d/direnv.fish
+++ dot_config/fish/conf.d/direnv.fish
@@ -1,9 +1,7 @@
 status is-interactive; or exit
 
 function __direnv_export_eval --on-variable PWD
-  if status is-command-substitution
-    return
-  end
-
   direnv export fish | source
 end
+
+__direnv_export_eval
