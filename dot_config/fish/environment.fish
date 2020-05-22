b01c9378e0fe6e5a2b96ecb9a2ed87d79945a1d2
Bjorn Neergaard <bjorn@neersighted.com>
2020-05-21 23:49:42 -0600

    fish: more robust MANPATH

--- dot_config/fish/environment.fish
+++ dot_config/fish/environment.fish
@@ -4,7 +4,6 @@ function def -a var val
   set -qUx $var; or set -gx $var $val
 end
 
-
 #
 # base
 #
@@ -103,7 +102,6 @@ def AWS_CONFIG_FILE $XDG_CONFIG_HOME/aws/config
 def AWS_SHARED_CREDENTIALS_FILE $XDG_CONFIG_HOME/aws/credentials
 
 # bat
-def BAT_STYLE full
 def BAT_THEME Nord
 def BAT_PAGER 'less --quit-if-one-screen --no-init'
 
@@ -172,11 +170,6 @@ def FZF_OPEN_OPTS "--preview='peek {}'"
 # (man)path
 #
 
-# default manpath
-set -q MANPATH; or set -x MANPATH ''
-
-# snap
-path_prepend PATH /snap/bin
 # ccache {linux, {bsd, macos}}
 path_prepend PATH /usr/{lib/ccache/bin,local/{libexec/ccache,opt/ccache/libexec}}
 
@@ -184,22 +177,40 @@ path_prepend PATH /usr/{lib/ccache/bin,local/{libexec/ccache,opt/ccache/libexec}
 path_prepend PATH $GOBIN $GOENV_ROOT/{shims,bin}
 # nodejs
 path_prepend PATH $NODENV_ROOT/{shims,bin}
-set -q NODENV_VERSION; or read -l NODENV_VERSION < $NODENV_ROOT/version
-path_prepend MANPATH $NODENV_ROOT/versions/$NODENV_VERSION/share/man
 # python
 path_prepend PATH $PIPX_BIN_DIR $POETRY_HOME/bin $PYENV_ROOT/{shims,bin}
-set -q PYENV_VERSION; or read -l PYENV_VERSION < $PYENV_ROOT/version
-path_prepend MANPATH $PYENV_ROOT/versions/$PYENV_VERSION/share/man
 # ruby
 path_prepend PATH $RBENV_ROOT/{shims,bin}
-set -q RBENV_VERSION; or read -l RBENV_VERSION < $RBENV_ROOT/version
-path_prepend MANPATH $RBENV_ROOT/versions/$RBENV_VERSION/share/man
 # rust
 path_prepend PATH $CARGO_HOME/bin
+# dotfiles/local
+path_prepend PATH $HOME/.local/bin
+
+#
+# manpath
+#
+
+# base manpath
+set -q MANPATH; or set -x MANPATH ''
+# nodejs
+if test -e $NODENV_ROOT/version
+  set -q NODENV_VERSION; or read -l NODENV_VERSION < $NODENV_ROOT/version
+  path_prepend MANPATH $NODENV_ROOT/versions/$NODENV_VERSION/share/man
+end
+# python
+if test -e $PYENV_ROOT/version
+  set -q PYENV_VERSION; or read -l PYENV_VERSION < $PYENV_ROOT/version
+  path_prepend MANPATH $PYENV_ROOT/versions/$PYENV_VERSION/share/man
+end
+# ruby
+if test -e $RBENV_ROOT/VERSION
+  set -q RBENV_VERSION; or read -l RBENV_VERSION < $RBENV_ROOT/version
+  path_prepend MANPATH $RBENV_ROOT/versions/$RBENV_VERSION/share/man
+end
+# rust
 set -q RUSTUP_TOOLCHAIN; or set -l RUSTUP_TOOLCHAIN (string match -r 'default_toolchain = "(.*)"' < $RUSTUP_HOME/settings.toml)[2]
 path_prepend MANPATH $RUSTUP_HOME/toolchains/$RUSTUP_TOOLCHAIN/{,share/}man
 # dotfiles/local
-path_prepend PATH $HOME/.local/bin
 path_prepend MANPATH $HOME/.local/share/man
 
 functions -e def
