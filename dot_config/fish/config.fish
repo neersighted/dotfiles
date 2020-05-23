22de526d5e30d194129a9ceccfef91c916089554
Bjorn Neergaard <bjorn@neersighted.com>
2020-05-23 00:47:43 -0600

    fish: optimize startup further

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,12 +1,11 @@
 # core fish configuration
+set -qU fish_greeting; or set -U fish_greeting
 set -qU fish_term24bit; or set -U fish_term24bit 1
 set -qU fish_key_bindings; or set -U fish_key_bindings fish_default_key_bindings
 set -qU fish_features; or set -U fish_features stderr-nocaret qmark-noglob
 
 # function subdirectories
-for dir in $__fish_config_dir/functions/*
-  test -d $dir; and set -p fish_function_path $dir
-end
+set -p fish_function_path (string trim -rc/ $__fish_config_dir/functions/**/)
 
 # environment setup
 source $__fish_config_dir/platform.fish
