0b4c26c227c2488a10b96c59384f9d2afd7250bf
Bjorn Neergaard <bjorn@neersighted.com>
2020-06-18 11:37:46 -0600

    tgdb: final form

--- private_dot_config/fish/functions/tgdb.fish
+++ private_dot_config/fish/functions/tgdb.fish
@@ -1,18 +1,16 @@
 function tgdb -d 'tmux-enabled gdb'
   if set -qx INNER
     set dashboard_pane (tmux split-window -PF '#{pane_id}|#{pane_tty}' -bh -d -t $TMUX_PANE -- 'exec disowntty' | string split '|')
-    set assembly_pane (tmux split-window -PF '#{pane_id}|#{pane_tty}' -bv -d -t $dashboard_pane[1] -- 'exec disowntty' | string split '|')
-    set source_pane (tmux split-window -PF '#{pane_id}|#{pane_tty}' -v -d -t $assembly_pane[1] -- 'exec disowntty' | string split '|')
-    set inferior_pane (tmux split-window -PF '#{pane_id}|#{pane_tty}' -bv -l 25% -d -t $TMUX_PANE -- 'exec disowntty' | string split '|')
-    set shell_pane (tmux split-window -PF '#{pane_id}' -v -d -t $TMUX_PANE)
+    set inferior_pane (tmux split-window -PF '#{pane_id}|#{pane_tty}' -v -l 34% -d -t $dashboard_pane[1] -- 'exec disowntty' | string split '|')
+    set source_pane (tmux split-window -PF '#{pane_id}|#{pane_tty}' -bv -l 66% -d -t $TMUX_PANE -- 'exec disowntty' | string split '|')
+    set assembly_pane (tmux split-window -PF '#{pane_id}|#{pane_tty}' -bv -d -t $source_pane[1] -- 'exec disowntty' | string split '|')
 
-    gdb -x $XDG_CONFIG_HOME/tgdb/gdbinit -tty $inferior_pane[2] -ex 'dashboard -output '$dashboard_pane[2] -ex 'dashboard assembly -output '$assembly_pane[2] -ex 'dashboard source -output '$source_pane[2] -ex 'start' -quiet $argv
+    gdb -x $XDG_CONFIG_HOME/tgdb/gdbinit -tty $inferior_pane[2] -ex 'dashboard -output '$dashboard_pane[2] -ex 'dashboard source -output '$source_pane[2] -ex 'dashboard assembly -output '$assembly_pane[2] -ex 'start' -quiet $argv
 
     tmux kill-pane -t $dashboard_pane[1]
     tmux kill-pane -t $assembly_pane[1]
     tmux kill-pane -t $source_pane[1]
     tmux kill-pane -t $inferior_pane[1]
-    tmux kill-pane -t $shell_pane
   else
     is_tmux; and set cmd new-window; or set cmd new
     tmux $cmd -n tgdb -e INNER=1 -- tgdb $argv
