544505576218bb4bc001360fb7cade4682a1c188
Bjorn Neergaard <bjorn@neersighted.com>
2020-11-23 02:59:36 -0700

    fish: use pip --user completions

--- private_dot_config/fish/conf.d/core.fish
+++ private_dot_config/fish/conf.d/core.fish
@@ -3,3 +3,6 @@ set -qU fish_greeting; or set -U fish_greeting
 set -qU fish_term24bit; or set -U fish_term24bit 1
 set -qU fish_key_bindings; or set -U fish_key_bindings fish_default_key_bindings
 set -qU fish_features; or set -U fish_features stderr-nocaret qmark-noglob
+
+# fish path configuration
+set -p fish_complete_path $HOME/.local/etc/fish/completions
