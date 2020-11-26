b5b981f31115b84e0747b94c9f63ef6ade8517c5
Bjorn Neergaard <bjorn@neersighted.com>
2020-11-26 10:08:43 -0700

    fish: vendor argcomplete for pipx

--- private_dot_config/fish/completions/pipx.fish
+++ private_dot_config/fish/completions/pipx.fish
@@ -1,1 +1,17 @@
-register-python-argcomplete --shell fish pipx | source
+
+function __fish_pipx_complete
+    set -x _ARGCOMPLETE 1
+    set -x _ARGCOMPLETE_DFS \t
+    set -x _ARGCOMPLETE_IFS \n
+    set -x _ARGCOMPLETE_SUPPRESS_SPACE 1
+    set -x _ARGCOMPLETE_SHELL fish
+    set -x COMP_LINE (commandline -p)
+    set -x COMP_POINT (string length (commandline -cp))
+    set -x COMP_TYPE
+    if set -q _ARC_DEBUG
+        pipx 8>&1 9>&2 1>/dev/null 2>&1
+    else
+        pipx 8>&1 9>&2 1>&9 2>&1
+    end
+end
+complete -c pipx -f -a '(__fish_pipx_complete)'
