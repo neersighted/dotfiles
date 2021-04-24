f504d01e9fb609dbb7a421475da65162cc970e1e
Bjorn Neergaard <bjorn@neersighted.com>
2021-04-24 17:18:00 -0600

    fish: handle WSL1 socket checking

--- private_dot_config/fish/conf.d/crypto-agents.fish
+++ private_dot_config/fish/conf.d/crypto-agents.fish
@@ -3,23 +3,36 @@ is_ssh; and exit
 set -x GPG_TTY $tty
 
 if is_wsl
-  set -l netstat (ss --family=unix --listening)
+  if is_wsl1
+    function is_listening -a path
+      socat -u OPEN:/dev/null UNIX-CONNECT:$path &>/dev/null
+    end
+  else if is_wsl2
+    function is_listening -a path
+      set -q __netstat; or set -g netstat (ss --family=unix --listening)
+
+      string match -rq $path $netstat
+    end
+  end
 
   # make sure the windows gpg-agent is up
   setsid -f gpg-connect-agent.exe /bye &>/dev/null
 
   # connect ssh to windows gpg-agent via wsl2-ssh-pageant
   set -x SSH_AUTH_SOCK $HOME/.ssh/ssh-agent.sock
-  if not string match -rq $SSH_AUTH_SOCK $netstat
+  if not is_listening $SSH_AUTH_SOCK
     rm -f $SSH_AUTH_SOCK
     setsid -f socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:'wsl2-ssh-pageant.exe' &>/dev/null
   end
 
   # connect gpg to windows gpg-agent via wsl2-ssh-pageant
-  if not string match -rq $GNUPGHOME/S.gpg-agent $netstat
+  if not is_listening $GNUPGHOME/S.gpg-agent
     rm -f $GNUPGHOME/S.gpg-agent
     setsid -f socat UNIX-LISTEN:$GNUPGHOME/S.gpg-agent,fork EXEC:'wsl2-ssh-pageant.exe --gpg S.gpg-agent' &>/dev/null
   end
+
+  set -eg __netstat
+  functions -e is_listening
 else if not string match -rq 'S.gpg-agent.ssh$' $SSH_AUTH_SOCK
   # plumb ssh to gpg-agent
   set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
