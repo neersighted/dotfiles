7fa192aa65342a6a1627727d63a2f41bcfb08888
Bjorn Neergaard <bjorn@neersighted.com>
2021-05-06 11:53:15 -0600

    fish: strongly order init

--- private_dot_config/fish/conf.d/03_settings.fish
+++ private_dot_config/fish/conf.d/03_settings.fish
@@ -9,6 +9,12 @@ set -qx VISUAL; or set -Ux VISUAL $EDITOR
 # pager
 set -qx PAGER; or set -Ux PAGER less
 
+# fish
+set -qU fish_greeting; or set -U fish_greeting
+set -qU fish_term24bit; or set -U fish_term24bit 1
+set -qU fish_key_bindings; or set -U fish_key_bindings fish_default_key_bindings
+set -qU fish_features; or set -U fish_features stderr-nocaret qmark-noglob
+
 #
 # toolchains
 #
