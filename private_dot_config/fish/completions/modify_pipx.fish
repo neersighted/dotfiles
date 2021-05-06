064b685a83b44dc0c381482a5ee046b13772e160
Bjorn Neergaard <bjorn@neersighted.com>
2021-05-06 14:36:14 -0600

    chezmoi: fix pipx.fish script

--- private_dot_config/fish/completions/modify_pipx.fish
+++ private_dot_config/fish/completions/modify_pipx.fish
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 if [ -d "$PYENV_ROOT/versions/pipx" ]; then
-  PYENV_VERSION=pipx register-python-argcomplete --shell fish pipx
+  "$PYENV_ROOT"/versions/pipx/bin/register-python-argcomplete --shell fish pipx
 fi
