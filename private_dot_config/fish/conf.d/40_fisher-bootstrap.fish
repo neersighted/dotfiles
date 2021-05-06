bad5db74714f5c700ec93b52e5b5920f8d3ed9e9
Bjorn Neergaard <bjorn@neersighted.com>
2021-05-06 13:59:00 -0600

    fish: use rebind for mappings

--- private_dot_config/fish/conf.d/40_fisher-bootstrap.fish
+++ private_dot_config/fish/conf.d/40_fisher-bootstrap.fish
@@ -1,6 +1,10 @@
 status is-interactive; or exit
 
-if not functions -q fisher
-  curl -sSL https://git.io/fisher | source
-  and fisher update
+function __fisher_bootstrap --on-event fish_startup
+  if not functions -q fisher
+    curl -sSL https://git.io/fisher | source
+    and fisher update
+  end
+
+  functions -e __fisher_bootstrap
 end
---
+++ private_dot_config/fish/conf.d/60_binds.fish
@@ -0,0 +1,25 @@
+status is-interactive; or exit
+
+function rebind -a old new
+  set cmd (bind --user $old | string replace -r '^bind (--user )?\S+ ' '')
+  bind --erase $old
+  bind $new $cmd
+end
+
+function __bind_keys --on-event fish_startup
+  bind \r repaint-and-execute
+  bind \n repaint-and-execute
+
+  bind ! history-prev-command
+  bind \$ history-prev-arguments
+
+  bind \er push-line
+
+  bind \ex tmx
+  bind \ez fzfz
+
+  rebind \cf \co # fzf directory
+  rebind \cv \ev # fzf variables
+
+  functions -e __bind_keys
+end
