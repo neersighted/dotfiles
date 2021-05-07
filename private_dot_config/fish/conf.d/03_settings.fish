c85e9aaa52c779501ae07f3044a7334a436ef5ad
Bjorn Neergaard <bjorn@neersighted.com>
2021-05-06 21:02:35 -0600

    fish: use 3.1 features

--- private_dot_config/fish/conf.d/03_settings.fish
+++ private_dot_config/fish/conf.d/03_settings.fish
@@ -13,7 +13,7 @@ set -qx PAGER; or set -Ux PAGER less
 set -qU fish_greeting; or set -U fish_greeting
 set -qU fish_term24bit; or set -U fish_term24bit 1
 set -qU fish_key_bindings; or set -U fish_key_bindings fish_default_key_bindings
-set -qU fish_features; or set -U fish_features stderr-nocaret qmark-noglob
+set -qU fish_features; or set -U fish_features 3.1
 
 #
 # toolchains
