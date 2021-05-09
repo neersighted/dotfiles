dc728e67bc8f0a0e945c89fb9ebaa809bd8063d6
Bjorn Neergaard <bjorn@neersighted.com>
2021-05-08 21:53:18 -0600

    terminfo: rework slightly using chezmoiignore

--- private_dot_terminfo/run_once_terminfo.sh
+++ private_dot_terminfo/run_once_terminfo.sh
@@ -1,4 +1,3 @@
-{{- if eq .chezmoi.os "darwin" -}}
 #!/bin/sh
 
 if ! /usr/bin/infocmp tmux-256color >/dev/null 2>&1; then
@@ -9,5 +8,3 @@ if ! /usr/bin/infocmp tmux-256color >/dev/null 2>&1; then
     | sed 's/pairs#0x10000/pairs#0x1000/' > "$terminfo"
   /usr/bin/tic -x "$terminfo"
 fi
-
-{{- end -}}
