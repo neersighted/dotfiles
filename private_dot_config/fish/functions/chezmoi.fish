2649ba074c4a38f384a94615430d05b71a3ac124
Bjorn Neergaard <bjorn@neersighted.com>
2021-05-09 00:04:02 -0600

    chezmoi: smart fzf'd edit wrapper

--- private_dot_config/fish/functions/chezmoi.fish
+++ private_dot_config/fish/functions/chezmoi.fish
@@ -4,6 +4,14 @@ function chezmoi
   switch $argv[1]
   case cd
     cd (chezmoi source-path)
+  case edit
+    if test (count $argv) -eq 1
+      if set targets (chezmoi managed --include=files | fzf --multi)
+        chezmoi edit ~/$targets
+      end
+    else
+      command chezmoi $argv
+    end
   case '*'
     command chezmoi $argv
   end
