552acf70533a57e0a47f34c6e369190b2299ec88
Bjorn Neergaard <bjorn@neersighted.com>
2021-05-09 00:15:01 -0600

    fish: add more abbrs

--- private_dot_config/fish/conf.d/40_abbr.fish
+++ private_dot_config/fish/conf.d/40_abbr.fish
@@ -4,4 +4,13 @@ abbr -a -g lt 'll -TL2'
 
 abbr -a -g chez 'chezmoi'
 abbr -a -g cad 'chezmoi add'
+abbr -a -g cap 'chezmoi apply'
 abbr -a -g ced 'chezmoi edit'
+abbr -a -g cup 'chezmoi update'
+
+abbr -a -g gco 'git commit'
+abbr -a -g gfe 'git fetch'
+abbr -a -g gpl 'git pull'
+abbr -a -g gpu 'git push'
+abbr -a -g gre 'git rebase'
+abbr -a -g gsw 'git switch'
