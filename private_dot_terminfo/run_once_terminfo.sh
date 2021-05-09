4296226d55d8e16f4c4c9750e83574b4cd57dbae
Bjorn Neergaard <bjorn@neersighted.com>
2021-05-09 04:27:37 -0600

    terminfo: add more entries on macos

--- private_dot_terminfo/run_once_terminfo.sh
+++ private_dot_terminfo/run_once_terminfo.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-if ! /usr/bin/infocmp tmux-256color >/dev/null 2>&1; then
-  terminfo=$(mktemp)
-  trap 'rm $terminfo' EXIT
+for term in alacritty-direct iterm2 tmux-256color; do
+  if ! /usr/bin/infocmp $term >/dev/null 2>&1; then
+    terminfo=$(mktemp)
+    trap 'rm $terminfo' EXIT
 
-  /usr/local/opt/ncurses/bin/infocmp -x tmux-256color \
-    | sed 's/pairs#0x10000/pairs#0x1000/' > "$terminfo"
-  /usr/bin/tic -x "$terminfo"
-fi
+    /usr/local/opt/ncurses/bin/infocmp -x $term \
+      | sed 's/pairs#0x10000/pairs#0x1000/' > "$terminfo"
+    /usr/bin/tic -x "$terminfo"
+  fi
+done
