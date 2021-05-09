e63f38230e1c235d8d740c19c3176aeafe326992
Bjorn Neergaard <bjorn@neersighted.com>
2021-05-09 00:17:09 -0600

    fish: fisher abbr

--- private_dot_config/fish/conf.d/40_abbr.fish
+++ private_dot_config/fish/conf.d/40_abbr.fish
@@ -2,12 +2,20 @@ status is-interactive; or exit
 
 abbr -a -g lt 'll -TL2'
 
+# chezmoi
 abbr -a -g chez 'chezmoi'
 abbr -a -g cad 'chezmoi add'
 abbr -a -g cap 'chezmoi apply'
 abbr -a -g ced 'chezmoi edit'
 abbr -a -g cup 'chezmoi update'
 
+# fisher
+abbr -a -g fad 'fisher install'
+abbr -a -g fls 'fisher list'
+abbr -a -g frm 'fisher uninstall'
+abbr -a -g fup 'fisher update'
+
+# git
 abbr -a -g gco 'git commit'
 abbr -a -g gfe 'git fetch'
 abbr -a -g gpl 'git pull'
