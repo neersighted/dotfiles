2317c2f9d379609ad194781932e5eb7d34fa96e7
Bjorn Neergaard <bjorn@neersighted.com>
2021-05-09 00:11:54 -0600

    chezmoi: do the same for add

--- private_dot_config/fish/functions/chezmoi.fish
+++ private_dot_config/fish/functions/chezmoi.fish
@@ -12,6 +12,18 @@ function chezmoi
     else
       command chezmoi $argv
     end
+  case add
+    if test (count $argv) -eq 1
+      if set modified (
+        chezmoi status \
+        | string match -re '^M' | string split -f 2 ' ' \
+        | fzf --multi --preview 'chezmoi diff --color=true ~/{}'
+      )
+        chezmoi add ~/$modified
+      end
+    else
+      command chezmoi $argv
+    end
   case '*'
     command chezmoi $argv
   end
