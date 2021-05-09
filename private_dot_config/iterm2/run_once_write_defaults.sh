280251d8fd721a1dbb717bf07f40b378acb478f9
Bjorn Neergaard <bjorn@neersighted.com>
2021-05-08 23:11:15 -0600

    iterm2: add to dotfiles

---
+++ private_dot_config/iterm2/run_once_write_defaults.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# set path to config files
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "${XDG_CONFIG_HOME:-~/.config}/iterm2"
+# load settings from config directory
+defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
+# save settings automatically
+defaults write com.googlecode.iterm2.plist NoSyncNeverRemindPrefsChangesLostForFile -bool true
+defaults write com.googlecode.iterm2.plist NoSyncNeverRemindPrefsChangesLostForFile_selection -int 2
