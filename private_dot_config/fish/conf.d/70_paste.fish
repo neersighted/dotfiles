9c8c4ab196ed233621782fdf2eb75b793314ceb3
Bjorn Neergaard <bjorn@neersighted.com>
2021-05-13 23:00:38 -0600

    clipboard-provider: fix multiline and multi-provider bug

--- private_dot_config/fish/conf.d/70_paste.fish
+++ private_dot_config/fish/conf.d/70_paste.fish
@@ -2,9 +2,11 @@ status is-interactive; or exit
 
 # wrappers to make fish_clipboard_copy/fish_clipboard_paste work with clipboard-provider
 function pbcopy
+  set -lx COPY_PROVIDERS desktop tmux
   clipboard-provider copy
 end
 
 function pbpaste
+  set -lx PASTE_PROVIDERS desktop tmux
   clipboard-provider paste
 end
