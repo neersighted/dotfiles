95973928b0284ba1464559f05735a3a2b6df4169
Bjorn Neergaard <bjorn@neersighted.com>
2021-05-15 10:32:32 -0600

    fish: more abbrs

--- private_dot_config/fish/conf.d/40_abbr.fish
+++ private_dot_config/fish/conf.d/40_abbr.fish
@@ -16,12 +16,20 @@ abbr -q frm; or abbr -a -- frm 'fisher remove'
 abbr -q fup; or abbr -a -- fup 'fisher update'
 
 # git
+abbr -q gad; or abbr -a -- gad 'git add'
+abbr -q gbr; or abbr -a -- gbr 'git branch'
 abbr -q gco; or abbr -a -- gco 'git commit'
 abbr -q gfe; or abbr -a -- gfe 'git fetch'
+abbr -q gin; or abbr -a -- gin 'git init'
 abbr -q gpl; or abbr -a -- gpl 'git pull'
 abbr -q gpu; or abbr -a -- gpu 'git push'
 abbr -q gre; or abbr -a -- gre 'git rebase'
+abbr -q grm; or abbr -a -- grm 'git rm'
 abbr -q gsw; or abbr -a -- gsw 'git switch'
+
+# gh
+abbr -q gpc; or abbr -a -- gpc 'gh pr create'
+abbr -q grc; or abbr -a -- grc 'gh repo clone'
 
 # tmux
 abbr -q tdp; or abbr -a -- tdp 'tmux display-message -p'
