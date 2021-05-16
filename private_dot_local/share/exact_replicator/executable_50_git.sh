1d4b8b0ac613819e2f4be40175410a3c9108e6e5
Bjorn Neergaard <bjorn@neersighted.com>
2021-05-16 00:19:20 -0600

    replicator: fix arch, use packaged interactive-rebase-tool, remove fac

--- private_dot_local/share/exact_replicator/executable_50_git.sh
+++ private_dot_local/share/exact_replicator/executable_50_git.sh
@@ -7,9 +7,6 @@ set -e
 
 section "Git"
 
-cargo_install "git-interactive-rebase-tool"
-go_get "github.com/mkchoi212/fac"
-
 github_sync "kamranahmedse/git-standup" "git-standup" "$HOME/.local/bin/git-standup" true
 github_sync "paulirish/git-open" "git-open" "$HOME/.local/bin/git-open" true
 github_sync "paulirish/git-recent" "git-recent" "$HOME/.local/bin/git-recent" true
