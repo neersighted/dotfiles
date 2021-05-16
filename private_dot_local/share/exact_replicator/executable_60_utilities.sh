469e0e81a85c9a4c5e7c511ae419aaa80f82223a
Bjorn Neergaard <bjorn@neersighted.com>
2021-05-16 00:28:33 -0600

    replicator: ditch ngrok

--- private_dot_local/share/exact_replicator/executable_60_utilities.sh
+++ private_dot_local/share/exact_replicator/executable_60_utilities.sh
@@ -17,23 +17,7 @@ pipx_install "gdbgui"
 # http-prompt
 pipx_install "http-prompt"
 
-# ngrok
-if ! command -v ngrok >/dev/null; then
-  goversion=$(go version | awk '{print $4}')
-  GOOS=${goversion%/*}
-  GOARCH=${goversion#*/}
-
-  important "Bootstrapping ngrok..."
-  curl -sS "https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-$GOOS-$GOARCH.zip" | funzip >"$HOME/.local/bin/ngrok"
-  chmod +x "$HOME/.local/bin/ngrok"
-  ngrok update
-else
-  important "Self-updating ngrok..."
-  ngrok update
-fi
-
 # Pipeline Tools
-cargo_install "runiq"
 pipx_install "shell-functools"
 go_get "mvdan.cc/xurls/v2/cmd/xurls"
 
