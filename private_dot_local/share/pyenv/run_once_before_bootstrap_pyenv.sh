f35501b9de1fa3cece4bcdd6800a596b2a733876
Bjorn Neergaard <bjorn@neersighted.com>
2021-05-22 23:41:55 -0600

    pyenv: remove virtualenv plugin

--- private_dot_local/share/pyenv/run_once_before_bootstrap_pyenv.sh
+++ private_dot_local/share/pyenv/run_once_before_bootstrap_pyenv.sh
@@ -3,7 +3,6 @@
 if [ "$(basename "$PWD")" != 'pyenv' ]; then
   echo "Bootstrapping pyenv..."
   git clone https://github.com/pyenv/pyenv
-  git clone https://github.com/pyenv/pyenv-virtualenv pyenv/plugins/pyenv-virtualenv
   echo "Building native extensions..."
   pyenv/src/configure
   make -C pyenv/src
