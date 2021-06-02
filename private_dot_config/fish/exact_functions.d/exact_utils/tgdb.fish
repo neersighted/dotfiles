1baae9105ae3d7c5ae2c05448bf378c91bf4b300
Bjorn Neergaard <bjorn@neersighted.com>
2021-06-02 00:48:29 -0600

    tgdb: rust/python support, more improvements

--- private_dot_config/fish/exact_functions.d/exact_utils/tgdb.fish
+++ private_dot_config/fish/exact_functions.d/exact_utils/tgdb.fish
@@ -5,16 +5,55 @@ function tgdb -d 'tmux-enabled gdb(-dashboard)'
     set source_tty (tmux split-window -d -PF '#{pane_tty}' -bv -l 66% -t '{left}' -- 'exec disowntty')
     set assembly_tty (tmux split-window -d -PF '#{pane_tty}' -bv -t '{top-left}' -- 'exec disowntty')
 
-    gdb -x $XDG_CONFIG_HOME/tgdb/gdbinit \
-       -tty $inferior_tty \
+    function __tgdb_rust
+      command -q rustc; or return
+
+      # use rustc to locate the rustlib folder for the current toolchain
+      set rustlib (rustc --print=sysroot)/lib/rustlib
+      set src_path $rustlib/src/rust
+      set load_path $rustlib/etc
+
+      # add load_path to PYTHONPATH so import works
+      set -lx -p PYTHONPATH $load_path
+      string join -- \n \
+         -directory $src_path \
+         -ex "add-auto-load-safe-path $load_path"
+    end
+
+    function __tgdb_python
+      command -q pyenv; or return
+
+      # use pyenv to locate the pythonX-gdb.py file
+      # python-build installs it next to the python binaries
+      set load_path (dirname (pyenv which python))
+
+      string join -- \n \
+         -ex "add-auto-load-safe-path $load_path"
+    end
+
+    # run these before gdb so they can mutate the env
+    set rust_args (__tgdb_rust)
+    set python_args (__tgdb_python)
+
+    # use the tgdb-specific inputrc
+    set -x INPUTRC $XDG_CONFIG_HOME/tgdb/inputrc
+
+    gdb -tty $inferior_tty \
+       -x $XDG_CONFIG_HOME/tgdb/gdbinit \
+       $rust_args \
+       $python_args \
        -ex "dashboard -output $dashboard_tty" \
        -ex "dashboard source -output $source_tty" \
        -ex "dashboard assembly -output $assembly_tty" \
        -ex 'start' -quiet $argv
 
+    # mop up 'disowntty' instances
     for pid in (tmux list-panes -F '#{pane_pid}' -f "#{!=:#{pane_pid},$fish_pid}")
       kill $pid
     end
+
+    # and kill all the panes to be sure
+    tmux kill-pane -a -t $TMUX_PANE
   else
     is_tmux; and set cmd new-window; or set cmd new-session
     tmux $cmd -n tgdb -e TGDB_INNER=1 -- tgdb $argv
