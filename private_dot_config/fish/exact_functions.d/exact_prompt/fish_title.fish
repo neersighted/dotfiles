7809cdbd3aacdc32b565328f1ea72a53ba23e957
Bjorn Neergaard <bjorn@neersighted.com>
2021-11-28 19:40:29 -0700

    fish/tmux: redesign titles

--- private_dot_config/fish/exact_functions.d/exact_prompt/fish_title.fish
+++ private_dot_config/fish/exact_functions.d/exact_prompt/fish_title.fish
@@ -1,15 +1,11 @@
 function fish_title
   set -q argv[1]; or set argv 'fish'
 
-  if is_ssh
-    printf '(%s) ' (prompt_hostname)
-  end
-
   if test "$PWD" = "$HOME"
     set pwd $HOME
   else
     set pwd (prompt_pwd)
   end
 
-  printf '%s: %s' $pwd $argv
+  printf '%s: %s' $argv $pwd
 end
