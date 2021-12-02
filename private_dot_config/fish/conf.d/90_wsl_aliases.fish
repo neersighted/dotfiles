a18d5c711905e74e3cd821f74774b88af4e72d78
Bjorn Neergaard <bjorn@neersighted.com>
2021-12-01 21:05:04 -0700

    fish: optimize creation of wsl aliases

--- private_dot_config/fish/conf.d/90_wsl_aliases.fish
+++ private_dot_config/fish/conf.d/90_wsl_aliases.fish
@@ -1,15 +1,25 @@
 is_wsl; or exit
 
 set -l aliases \
+  cmd \
   multipass \
   neovide \
+  powershell \
+  wsl \
   ykman
 set -l alias_commands \
+  "cmd.exe" \
   "multipass.exe" \
   "neovide.exe --wsl" \
+  "powershell.exe" \
+  "wsl.exe" \
   "ykman.exe"
 
 while count $aliases >/dev/null
-  alias $aliases[1] $alias_commands[1]
-  set -e aliases[1]; set -e alias_commands[1] # FIXME: old fish can't erase two variables in one command
+  set -l alias $aliases[1]; set -e aliases[1]
+  set -l command (string split ' ' $alias_commands[1]); set -e alias_commands[1]
+
+  printf 'function %s -d "wsl wrapper for %s" -w %s; %s $argv; end' \
+     $alias $command[1] $command[1] "$command" \
+ | source
 end
