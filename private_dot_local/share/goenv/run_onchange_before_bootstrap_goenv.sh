ec0f76317aee716a539c7b6a03b50af8f5a68737
Bjorn Neergaard <bjorn@neersighted.com>
2021-12-03 09:18:44 -0700

    xxenv: use similar -latest strategy

--- private_dot_local/share/goenv/run_onchange_before_bootstrap_goenv.sh
+++ private_dot_local/share/goenv/run_onchange_before_bootstrap_goenv.sh
@@ -3,6 +3,8 @@
 if [ "$(basename "$PWD")" != 'goenv' ]; then
   echo "Bootstrapping goenv..."
   git clone https://github.com/syndbg/goenv
+  git clone https://github.com/momo-lab/xxenv-latest goenv/plugins/xxenv-latest
+
   echo "Building native extensions..."
   goenv/src/configure
   make -C goenv/src
--- private_dot_local/share/rbenv/run_once_before_bootstrap_rbenv.sh
+++ private_dot_local/share/rbenv/run_once_before_bootstrap_rbenv.sh
@@ -5,6 +5,8 @@ if [ "$(basename "$PWD")" != 'rbenv' ]; then
   git clone https://github.com/rbenv/rbenv
   git clone https://github.com/rbenv/ruby-build rbenv/plugins/ruby-build
   git clone https://github.com/rbenv/rbenv-default-gems rbenv/plugins/rbenv-default-gems
+  git clone https://github.com/momo-lab/xxenv-latest rbenv/plugins/xxenv-latest
+
   echo "Building native extensions..."
   rbenv/src/configure
   make -C rbenv/src
