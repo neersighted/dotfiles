c28740a5bef1eec3134d220a60fc4c8ac64d9828
Bjorn Neergaard <bjorn@neersighted.com>
2021-12-04 07:51:54 -0700

    nodenv: add alias support

--- private_dot_local/share/nodenv/run_once_before_bootstrap_nodenv.sh
+++ private_dot_local/share/nodenv/run_once_before_bootstrap_nodenv.sh
@@ -4,6 +4,7 @@ if [ "$(basename "$PWD")" != 'nodenv' ]; then
   echo "Bootstrapping nodenv..."
   git clone https://github.com/nodenv/nodenv
   git clone https://github.com/nodenv/node-build nodenv/plugins/node-build
+  git clone https://github.com/nodenv/nodenv-aliases nodenv/plugins/nodenv-aliases
   git clone https://github.com/momo-lab/xxenv-latest nodenv/plugins/xxenv-latest
   echo "Building native extensions..."
   nodenv/src/configure
