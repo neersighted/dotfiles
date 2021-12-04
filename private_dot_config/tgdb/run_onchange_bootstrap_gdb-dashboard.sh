6fa24af09ab451fd82981f97c14f1a6663d92550
Bjorn Neergaard <bjorn@neersighted.com>
2021-12-04 08:29:53 -0700

    gdb: manage gdb-dashboard with chezmoiexternal

--- private_dot_config/tgdb/run_onchange_bootstrap_gdb-dashboard.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -e
-
-if [ ! -d "${XDG_DATA_HOME:-$HOME/.local/share}/gdb-dashboard" ]; then
-  echo "Bootstrapping gdb-dashboard..."
-  git clone https://github.com/cyrus-and/gdb-dashboard "${XDG_DATA_HOME:-$HOME/.local/share}/gdb-dashboard"
-fi
-
-# vi: ft=sh
