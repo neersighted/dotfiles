ddabe62b8aea52718dbf69772e1cb4262adf2593
Bjorn Neergaard <bjorn@neersighted.com>
2021-12-05 11:52:03 -0700

    fish: tweak path abbrev threshold

--- private_dot_config/fish/exact_functions.d/exact_prompt/prompt_pwd.fish
+++ private_dot_config/fish/exact_functions.d/exact_prompt/prompt_pwd.fish
@@ -1,5 +1,5 @@
 function prompt_pwd
-    set -q prompt_pwd_threshold; or set prompt_pwd_threshold 60
+    set -q prompt_pwd_threshold; or set prompt_pwd_threshold 32
     set -q prompt_pwd_dir_length; or set prompt_pwd_dir_length 3
     set -q prompt_pwd_anchors; or set prompt_pwd_anchors .bzr .citc .git .hg .node-version .python-version .go-version .ruby-version .lua-version .java-version .perl-version .php-version .tool-version .shorten_folder_marker .svn .terraform CVS Cargo.toml composer.json go.mod package.json stack.yaml
 
