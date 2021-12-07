e42162730f82dde576b0d8b56fd30010dd15b75f
Bjorn Neergaard <bjorn@neersighted.com>
2021-12-06 23:40:41 -0700

    ssh: add github IPs and script to generate them

---
+++ private_dot_ssh/.github_ips.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import ipaddress
+import itertools
+import json
+import urllib.request
+
+with urllib.request.urlopen("https://api.github.com/meta") as response:
+    meta = json.loads(response.read())
+    networks = [ipaddress.ip_network(cidr) for cidr in meta["git"]]
+
+    hosts = []
+    for network in networks:
+        if isinstance(network, ipaddress.IPv6Network):
+            if network.prefixlen < 32:
+                networks.extend(network.subnets(new_prefix=32))
+            else:
+                hosts.append(f"{network.network_address}*")
+        else:
+            if network.prefixlen < 24:
+                networks.extend(network.subnets(new_prefix=24))
+            elif network.prefixlen == 24:
+                prefix = str(network.network_address).rpartition('.')[0]
+                hosts.append(f"{prefix}.*")
+            else:
+                hosts.extend((str(host) for host in network.hosts()))
+
+    print(json.dumps(hosts, indent=2, sort_keys=True))
