acd1fafc0bb4452000cef3649e2385d4554904c9
Bjorn Neergaard <bjorn@neersighted.com>
2022-01-16 00:58:33 -0700

    starship: supress install script output

--- .chezmoiscripts/run_install_starship.sh
+++ .chezmoiscripts/run_install_starship.sh
@@ -4,5 +4,5 @@ export XDG_BIN_HOME="${XDG_BIN_HOME:-$HOME/.local/bin}"
 
 if [ ! "$(command -v starship)" ] || [ "$(dirname "$(command -v starship)")" = "$XDG_BIN_HOME" ]; then
   echo "Updating Starship..."
-  curl -fsSL https://starship.rs/install.sh | sh -s -- -b "$XDG_BIN_HOME" -y
+  curl -fsSL https://starship.rs/install.sh | sh -s -- -b "$XDG_BIN_HOME" -y >/dev/null
 fi
