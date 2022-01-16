8f34da0f3c2c4fe041b000a7d44967512ffb1cca
Bjorn Neergaard <bjorn@neersighted.com>
2022-01-16 09:11:25 -0700

    chezmoi: template starship install script

--- .chezmoiscripts/run_install_starship.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-export XDG_BIN_HOME="${XDG_BIN_HOME:-$HOME/.local/bin}" 
-
-if [ ! "$(command -v starship)" ] || [ "$(dirname "$(command -v starship)")" = "$XDG_BIN_HOME" ]; then
-  echo "Updating Starship..."
-  curl -fsSL https://starship.rs/install.sh | sh -s -- -b "$XDG_BIN_HOME" -y >/dev/null
-fi
