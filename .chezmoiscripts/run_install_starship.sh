84d5038c5df378ea68e5f21a075a23f91682031f
Bjorn Neergaard <bjorn@neersighted.com>
2022-01-16 00:47:55 -0700

    chezmoi: install starship on unpackaged platforms

---
+++ .chezmoiscripts/run_install_starship.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+export XDG_BIN_HOME="${XDG_BIN_HOME:-$HOME/.local/bin}" 
+
+if [ ! "$(command -v starship)" ] || [ "$(dirname "$(command -v starship)")" = "$XDG_BIN_HOME" ]; then
+  echo "Updating Starship..."
+  curl -fsSL https://starship.rs/install.sh | sh -s -- -b "$XDG_BIN_HOME" -y
+fi
