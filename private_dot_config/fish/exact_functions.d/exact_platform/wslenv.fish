2ee6d9c4a0b8075a6fa7e29e2970bd4d095358bc
Bjorn Neergaard <bjorn@neersighted.com>
2022-04-25 22:24:12 -0600

    fish: add display function to wslenv helper

--- private_dot_config/fish/exact_functions.d/exact_platform/wslenv.fish
+++ private_dot_config/fish/exact_functions.d/exact_platform/wslenv.fish
@@ -7,14 +7,18 @@ function wslenv
   set -a options (fish_opt --short=l --long=list)
 
   argparse $options -- $argv; or return
-  set -q argv[1]; and set name $argv[1]; or return
   set -q _flag_p; and set -a flags p
   set -q _flag_l; and set -a flags l
 
+  if not set -q argv[1]
+    set -S WSLENV
+    return
+  end
+
   if set -q flags
-    set fullname (printf '%s/%s' $name (string join '' $flags))
+    set fullname (printf '%s/%s' $argv[1] (string join '' $flags))
   else
-    set fullname $name
+    set fullname $argv[1]
   end
 
   if not contains $fullname $WSLENV
