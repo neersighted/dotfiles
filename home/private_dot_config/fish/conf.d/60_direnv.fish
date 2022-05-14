01bf90f7b9de56e3fae8aae2d43c2630e7cd6a9e
Bjorn Neergaard <bjorn@neersighted.com>
2022-05-14 01:53:00 -0600

    fish: futher tidying and optimization

--- home/private_dot_config/fish/conf.d/60_direnv.fish
+++
@@ -1,9 +0,0 @@
-status is-interactive; and command -q direnv; or exit
-
-function __direnv_export_eval --on-variable PWD
-  direnv export fish | source
-end
-
-function __direnv_export_eval_startup --on-event fish_startup
-  __direnv_export_eval
-end
