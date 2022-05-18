c56582c47c66de58a7181c5a27ab5fffc29bb0f9
Bjorn Neergaard <bjorn@neersighted.com>
2022-05-17 22:29:31 -0600

    fish: fix bad failback in poetry venv detection

--- home/private_dot_config/fish/conf.d/60_poetry.fish
+++ home/private_dot_config/fish/conf.d/60_poetry.fish
@@ -9,7 +9,7 @@ function __poetry_auto_activate --on-variable PWD
 
   if not set -q VIRTUAL_ENV; and set pyproject (upcate pyproject.toml); and string match -q '[tool.poetry]' < $pyproject
     set project (string replace -r '^(.+)/[^/]+$' '$1' $pyproject)
-    set venv (poetry env info --path; or echo $path/.venv)
+    set venv (poetry env info --path); or set venv $project/.venv
 
     if test -e $venv
       source "$venv/bin/activate.fish"
