640e8bf8bbe2292ce8343ab341f37109f7d9c6f1
Bjorn Neergaard <bjorn@neersighted.com>
2022-09-28 14:07:12 -0600

    git: make keycommand work without auth or gh cli

--- home/private_dot_config/git/executable_keycommand.sh
+++ home/private_dot_config/git/executable_keycommand.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
-LOADED_KEYS=$(ssh-add -L | cut -d' ' -f1,2)
-KNOWN_KEYS=$(gh api /user/ssh_signing_keys -q '.[].key')
+GITHUB_USER="neersighted"
+API_ENDPOINT="/users/$GITHUB_USER/ssh_signing_keys"
 
-printf 'key::%s' "$(comm -12 <(printf '%s' "$LOADED_KEYS" | sort) <(printf '%s' "$KNOWN_KEYS" | sort) | head -n1)"
+if command -v gh >/dev/null; then
+  known_keys=$(gh api $API_ENDPOINT -q '.[].key')
+else
+  known_keys=$(curl -fSsL https://api.github.com/$API_ENDPOINT | yq '.[].key')
+fi
+
+loaded_keys=$(ssh-add -L | cut -d' ' -f1,2)
+
+printf 'key::%s' "$(comm -12 <(printf '%s' "$loaded_keys" | sort) <(printf '%s' "$known_keys" | sort) | head -n1)"
