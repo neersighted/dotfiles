364e29a0ff81263ec649514ee5c1b972073e3913
Bjorn Neergaard <bjorn@neersighted.com>
2022-10-31 09:53:04 -0600

    pyenv: modernize with new latest command

--- home/private_dot_local/share/pyenv/run_once_before_bootstrap_pyenv.sh
+++ home/private_dot_local/share/pyenv/run_once_before_bootstrap_pyenv.sh
@@ -3,7 +3,6 @@
 if [ "$(basename "$PWD")" != 'pyenv' ]; then
   echo "Bootstrapping pyenv..."
   git clone https://github.com/pyenv/pyenv
-  git clone https://github.com/momo-lab/xxenv-latest pyenv/plugins/xxenv-latest
 
   echo "Building native extensions..."
   pyenv/src/configure
