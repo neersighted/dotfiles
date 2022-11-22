225680c706b7a1f3fc0413cb965425a7538aa97a
Bjorn Neergaard <bjorn@neersighted.com>
2022-11-21 21:31:52 -0700

    rbenv: drop extension build

--- home/private_dot_local/share/rbenv/run_once_before_bootstrap_rbenv.sh
+++ home/private_dot_local/share/rbenv/run_once_before_bootstrap_rbenv.sh
@@ -5,8 +5,4 @@ if [ "$(basename "$PWD")" != 'rbenv' ]; then
   git clone https://github.com/rbenv/rbenv
   git clone https://github.com/rbenv/ruby-build rbenv/plugins/ruby-build
   git clone https://github.com/momo-lab/xxenv-latest rbenv/plugins/xxenv-latest
-
-  echo "Building native extensions..."
-  rbenv/src/configure
-  make -C rbenv/src
 fi
