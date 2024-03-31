78d26cf97a350c2542385977a7abdd920906dd7f
Bjorn Neergaard <bjorn@neersighted.com>
2024-03-31 12:48:56 -0600

    fish: cleanup

--- home/private_dot_config/fish/exact_functions.d/exact_wrappers/sha256.fish
+++ home/private_dot_config/fish/exact_functions.d/exact_wrappers/sha256.fish
@@ -2,11 +2,11 @@ function sha256
   if command -q sha256
     command sha256 -q $argv
   else if command -q sha256sum
-    sha256sum $argv | string split -f1 "  "
+    sha256sum $argv | string split -f 1 '  '
   else if command -q shasum
-    shasum -a 256 $argv | string split -f1 "  "
+    shasum -a 256 $argv | string split -f 1 '  '
   else
-    echo "No sha256 available?"
+    echo 'No sha256 available?'
     return 1
   end
 end
