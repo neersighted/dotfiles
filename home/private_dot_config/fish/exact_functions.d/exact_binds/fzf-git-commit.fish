9b2ea3d0e41a3d0f817d2944d4acaa37b9989545
Bjorn Neergaard <bjorn@neersighted.com>
2025-10-10 15:49:14 -0600

    fish: simplify some aliases

--- home/private_dot_config/fish/exact_functions.d/exact_binds/fzf-git-commit.fish
+++ home/private_dot_config/fish/exact_functions.d/exact_binds/fzf-git-commit.fish
@@ -1,5 +1,5 @@
 function fzf-git-commit
-  set commit (git log --no-merges --color=always | fzf --scheme=history --query (commandline) | string split -f 1 ' ')
+  set commit (git log --no-merges --color=always | fzf --scheme=history | string split -f 1 ' ')
 
   commandline --insert $commit
 end
