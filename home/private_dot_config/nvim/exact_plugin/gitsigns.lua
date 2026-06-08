4470cbf89e8793d60733b0b7ef4008f2c68ecd00
Bjorn Neergaard <bjorn@neersighted.com>
2026-06-08 17:37:35 -0600

    gitsigns: add navigation bindings

--- home/private_dot_config/nvim/exact_plugin/gitsigns.lua
+++ home/private_dot_config/nvim/exact_plugin/gitsigns.lua
@@ -6,6 +6,9 @@ gitsigns.setup({
   },
 })
 
-vim.keymap.set('n', '<leader>B', function()
-  gitsigns.blame_line({ full = true })
-end, { desc = 'Blame current line (popup)' })
+vim.keymap.set('n', '<leader>B', function() gitsigns.blame_line({ full = true }) end, { desc = 'Blame current line (popup)' })
+
+vim.keymap.set('n', ']h', function() gitsigns.nav_hunk('next') end, { desc = 'Next git hunk' })
+vim.keymap.set('n', '[h', function() gitsigns.nav_hunk('prev') end, { desc = 'Previous git hunk' })
+vim.keymap.set('n', ']H', function() gitsigns.nav_hunk('last') end, { desc = 'Last git hunk' })
+vim.keymap.set('n', '[H', function() gitsigns.nav_hunk('first') end, { desc = 'First git hunk' })
