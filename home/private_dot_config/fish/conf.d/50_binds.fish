96781d022b734cfb39369662b99e34dd7e804366
Bjorn Neergaard <bjorn@neersighted.com>
2026-06-09 21:41:31 -0600

    fish: use fzf.fish for git log viewing

--- home/private_dot_config/fish/conf.d/50_binds.fish
+++ home/private_dot_config/fish/conf.d/50_binds.fish
@@ -8,10 +8,15 @@ function __bind_keys_startup --on-event fish_startup
 
   bind alt-r push-line
 
-  bind alt-x tmux-select
   bind alt-z zoxide-interactive
-
-  fzf_configure_bindings --directory=ctrl-o --variables=alt-v
+  bind alt-x tmux-select
   bind alt-g fzf-git-switch
-  bind ctrl-g fzf-git-commit
+
+  fzf_configure_bindings \
+    --directory=ctrl-o \
+    --git_log=ctrl-g \
+    --git_status=ctrl-alt-s \
+    --history=ctrl-r \
+    --processes=ctrl-alt-p \
+    --variables=alt-v
 end
--- home/private_dot_config/fish/exact_functions.d/exact_binds/fzf-git-commit.fish
+++
@@ -1,5 +0,0 @@
-function fzf-git-commit
-  set commit (git log --no-merges --color=always | fzf --scheme=history | string split -f 1 ' ')
-
-  commandline --insert $commit
-end
