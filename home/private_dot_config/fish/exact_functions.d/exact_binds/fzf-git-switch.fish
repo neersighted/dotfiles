1337c26075f155fa89c0ecc65452da6a31298e44
Bjorn Neergaard <bjorn@neersighted.com>
2026-06-09 21:58:24 -0600

    fish: minor fzf refinements

--- home/private_dot_config/fish/exact_functions.d/exact_binds/fzf-git-switch.fish
+++ home/private_dot_config/fish/exact_functions.d/exact_binds/fzf-git-switch.fish
@@ -1,8 +1,10 @@
 function fzf-git-switch
-  set branch (git branch --color=always --sort=committerdate | fzf --tac --no-sort --query (commandline))
+  if git rev-parse --git-dir >/dev/null 2>&1
+    set -f branch (git branch --color=always --sort=-committerdate | fzf --tmux center,80,20 --no-sort --query (commandline))
 
-  if test -n "$branch"
-    git checkout (builtin string trim --left --chars='* ' $branch)
+    if test -n "$branch"
+      git switch (builtin string trim --left --chars='* ' $branch)
+    end
   end
 
   commandline -f repaint
