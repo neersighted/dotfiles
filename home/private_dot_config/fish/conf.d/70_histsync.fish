f8917cd5e744d0238322089ab25dddde8fc6e59a
Bjorn Neergaard <bjorn@neersighted.com>
2026-06-10 19:08:19 -0600

    fish: fix histsync

--- home/private_dot_config/fish/conf.d/70_histsync.fish
+++ home/private_dot_config/fish/conf.d/70_histsync.fish
@@ -2,7 +2,8 @@ status is-interactive; or exit
 
 # shared history, but you can press up+enter for the last command
 # alternatively, an empty command will force a sync
-function __histsync --on-event fish_preexec
+function __histsync --on-event histsync
+  test -z "$fish_private_mode"; or return
   history save
   history merge
 end
--- home/private_dot_config/fish/exact_functions.d/exact_binds/execute-or-preexec.fish
+++ home/private_dot_config/fish/exact_functions.d/exact_binds/execute-or-preexec.fish
@@ -1,7 +1,7 @@
 function execute-or-preexec
   set -f commandline (commandline)
   if test -z "$commandline"
-    emit fish_preexec
+    emit histsync
   end
   commandline -f execute
 end
