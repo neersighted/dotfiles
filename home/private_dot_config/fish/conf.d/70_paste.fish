ed027b17b6c67268f07e524911a222444d28e0a0
Bjorn Neergaard <bjorn@neersighted.com>
2026-06-20 16:57:59 -0600

    fish: make clipboard-provider more robust; integrate directly

--- home/private_dot_config/fish/conf.d/70_paste.fish
+++ home/private_dot_config/fish/conf.d/70_paste.fish
@@ -1,12 +1,24 @@
 status is-interactive; or exit
 
-# wrappers to make fish_clipboard_copy/fish_clipboard_paste work with clipboard-provider
-function pbcopy
+
+function fish_clipboard_copy
   set -lx COPY_PROVIDERS desktop tmux
-  clipboard-provider copy
+  if isatty stdin
+    set -l cmdline (commandline --current-selection | fish_indent --only-indent | string collect)
+    test -n "$cmdline"; or set cmdline (commandline | fish_indent --only-indent | string collect)
+    printf '%s' $cmdline | clipboard-provider copy
+  else
+    clipboard-provider copy
+  end
 end
 
-function pbpaste
+function fish_clipboard_paste
   set -lx PASTE_PROVIDERS desktop tmux
-  clipboard-provider paste
+  set -l data (clipboard-provider paste | string collect -N)
+  string length -q -- "$data"; or return 1
+  if isatty stdout
+    __fish_paste $data
+  else
+    printf '%s' $data
+  end
 end
