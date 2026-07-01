627d47d3f1e1c9efcaf43d26751121e3ee0a685b
Bjorn Neergaard <bjorn@neersighted.com>
2026-07-01 06:11:42 -0600

    windows: polish preferences.ps1 a bit; drop unnecessary hash

--- home/.chezmoiscripts/run_onchange_after_windows_preferences.ps1
+++ home/.chezmoiscripts/run_onchange_after_windows_preferences.ps1
@@ -17,8 +17,10 @@ Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer
 
 # Add 'Reboot into UEFI Firmware' to the Desktop context menu.
 $UEFIMenu = 'HKLM:\Software\Classes\DesktopBackground\Shell\UEFI'
-$null = New-Item -Path "$UEFIMenu\command" -ItemType Directory -Force
-
+if (!(Test-Path -Path $UEFIMenu)) {
+    Write-Host 'Adding "Reboot into UEFI Firmware" to Desktop context menu...'
+    $null = New-Item -Path "$UEFIMenu\command" -ItemType Directory -Force
+}
 Set-ItemProperty -Path $UEFIMenu -Name 'Icon' -Value 'bootux.dll,-1016'
 Set-ItemProperty -Path $UEFIMenu -Name 'MUIVerb' -Value 'Reboot into UEFI Firmware'
 Set-ItemProperty -Path $UEFIMenu -Name 'Position' -Value 'Bottom'
@@ -51,12 +53,12 @@ foreach ($var in @('APPDATA/p', 'LOCALAPPDATA/p', 'USERPROFILE/p', 'TERMINAL_EMU
 }
 
 # Activate developer mode.
-$AppModelUnlock = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock'
-if (!(Test-Path -Path $AppModelUnlock)) {
+$DevMode = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock'
+if (!(Test-Path -Path $DevMode)) {
     Write-Host 'Enabling Developer Mode...'
-    $null = New-Item -Path $AppModelUnlock -ItemType Directory -Force
+    $null = New-Item -Path $DevMode -ItemType Directory -Force
 }
-Set-ItemProperty -Path $AppModelUnlock -Name AllowDevelopmentWithoutDevLicense -Value 1
+Set-ItemProperty -Path $DevMode -Name AllowDevelopmentWithoutDevLicense -Value 1
 
 # Enable WSL/Hyper-V.
 wsl --status
