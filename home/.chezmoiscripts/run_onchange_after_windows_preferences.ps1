1c5282dd8a1f655ce8860f745eee3feb7038191f
Bjorn Neergaard <bjorn@neersighted.com>
2026-07-01 00:41:42 -0600

    windows: fix up preferences script

--- home/.chezmoiscripts/run_onchange_after_windows_preferences.ps1
+++ home/.chezmoiscripts/run_onchange_after_windows_preferences.ps1
@@ -2,14 +2,11 @@
 if (!(new-object System.Security.Principal.WindowsPrincipal([System.Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)) {
     $elevated = new-object System.Diagnostics.ProcessStartInfo 'powershell.exe'
     $elevated.Arguments = $myInvocation.MyCommand.Definition + @('-NoProfile', '-ExecutionPolicy', 'Bypass', '-WindowStyle', 'hidden')
-    $elevated.Verb = 'runas'
+    $elevated.Verb = 'RunAs'
     $child = [System.Diagnostics.Process]::Start($elevated)
     $child.WaitForExit()
     return
 }
-
-# Create the HKCR: prefix for registry access.
-$null = New-PSDrive -PSProvider registry -Root HKEY_CLASSES_ROOT -Name HKCR
 
 # Set my shell preferences.
 Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name Hidden -Value 1
@@ -20,6 +17,16 @@ Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer
 Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name NavPaneShowAllFolders -Value 0
 Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name LaunchTo -Value 1
 Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name MMTaskbarMode -Value 2
+
+# Add 'Reboot into UEFI Firmware' to the Desktop context menu.
+$UEFIMenu = 'HKLM:\Software\Classes\DesktopBackground\Shell\UEFI'
+$null = New-Item -Path "$UEFIMenu\command" -ItemType Directory -Force
+
+Set-ItemProperty -Path $UEFIMenu -Name 'Icon' -Value 'bootux.dll,-1016'
+Set-ItemProperty -Path $UEFIMenu -Name 'MUIVerb' -Value 'Reboot into UEFI Firmware'
+Set-ItemProperty -Path $UEFIMenu -Name 'Position' -Value 'Bottom'
+Set-ItemProperty -Path $UEFIMenu -Name 'HasLUAShield' -Value ''
+Set-Item -Path "$UEFIMenu\command" -Value "shutdown.exe /r /fw /f /t 0"
 
 # Add %USERPROFILE%\bin to the PATH.
 $bin = "$env:USERPROFILE\bin"
@@ -54,28 +61,13 @@ if (!(Test-Path -Path $AppModelUnlock)) {
 }
 Set-ItemProperty -Path $AppModelUnlock -Name AllowDevelopmentWithoutDevLicense -Value 1
 
-# Add UEFI Firmware to the Desktop context menu.
-$UEFIMenu = 'HKCR:\DesktopBackground\Shell\UEFI'
-if (!(Test-Path -Path $UEFIMenu)) {
-    Write-Host 'Adding "Reboot into UEFI Firmware" to the Desktop context menu...'
-    $null = New-Item -Path $UEFIMenu -ItemType Directory -Force
-    $null = New-Item -Path $UEFIMenu'\command' -ItemType Directory -Force
+# Enable WSL/Hyper-V.
+wsl --status
+if ($LASTEXITCODE -ne 0) {
+    Write-Host 'Installing WSL...'
+    wsl --install --distribution archlinux
 }
-Set-ItemProperty -Path $UEFIMenu -Name Icon -Value 'bootux.dll,-1016'
-Set-ItemProperty -Path $UEFIMenu -Name MUIVerb -Value 'Reboot into UEFI Firmware'
-Set-ItemProperty -Path $UEFIMenu -Name Position -Value 'Bottom'
-Set-ItemProperty -Path $UEFIMenu'\command' -Name '(Default)' -Value "powershell -WindowStyle Hidden -NoProfile -Command `"Start-Process shutdown -ArgumentList '/r /f /t 0 /fw' -Verb runAs`""
-
-# Enable Hyper-V/WSL.
 if (!(Get-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-All).State) {
     Write-Host 'Enabling Hyper-V...'
     Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-All
-}
-if (!(Get-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform).State) {
-    Write-Host 'Enabling Virtual Machine Platform...'
-    Enable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform
-}
-if (!(Get-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux).State) {
-    Write-Host 'Enabling WSL...'
-    Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
 }
