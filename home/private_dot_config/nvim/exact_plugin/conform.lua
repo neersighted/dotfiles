7256df3938343a3699477b09405636dd39ba266a
Bjorn Neergaard <bjorn@neersighted.com>
2026-07-16 12:15:47 -0600

    nvim: drop manual conform overrides, now that ruff is used via LSP

--- home/private_dot_config/nvim/exact_plugin/conform.lua
+++ home/private_dot_config/nvim/exact_plugin/conform.lua
@@ -5,12 +5,3 @@ end
 -- Early register :Format and keybind; lazy load conform to keep startup fast.
 vim.api.nvim_create_user_command('Format', format, { desc = 'Format buffer via conform.nvim' })
 vim.keymap.set('n', '<leader>f', format, { desc = 'Format buffer' })
-vim.schedule(function()
-  -- Most filetypes use lsp_fallback (gopls, rust-analyzer, biome, etc. format natively).
-  -- Only list filetypes where the LSP doesn't ship a formatter or we want a different one.
-  require('conform').setup({
-    formatters_by_ft = {
-      python = { 'ruff_format', 'ruff_organize_imports' },
-    },
-  })
-end)
