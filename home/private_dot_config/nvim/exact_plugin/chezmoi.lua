5e16a6fd7770f428480cc49b317d80ecf8b63499
Bjorn Neergaard <bjorn@neersighted.com>
2026-07-17 00:06:20 -0600

    nvim: better chezmoi hooks

--- home/private_dot_config/nvim/exact_plugin/chezmoi.lua
+++
@@ -1,7 +0,0 @@
-vim.api.nvim_create_autocmd('BufWritePost', {
-  group = vim.api.nvim_create_augroup('chezmoi', { clear = true }),
-  pattern = vim.fn.expand('~') .. '/.local/share/chezmoi/*',
-  callback = function(args)
-    vim.system({ 'chezmoi', 'apply', '--exclude', 'scripts', '--source-path', args.file })
-  end,
-})
